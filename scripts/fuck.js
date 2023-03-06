65b534a0c1e470e64f7c155bdddfe04fa47d9ec1
swwind <i@sww.moe>
2023-03-06 14:51:49 +0800

    update: old post (2/4)

--- scripts/fuck.js
+++ scripts/fuck.js
@@ -5,23 +5,35 @@ for (const post of posts
   .sort(
     (b, a) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
   )
-  .filter((post) => new Date(post.createdAt).getFullYear() === 2022)) {
+  .filter((post) => new Date(post.createdAt).getFullYear() === 2021)) {
   const time = new Date(post.createdAt)
     .toLocaleString("zh-CN-u-ca-chinese", { dateStyle: "full" })
     .replace("十一月", "冬月")
     .slice(4, -3);
 
-  console.log(
-    JSON.stringify({ time, slot: post.slot, title: post.title }) + ","
-  );
+  console.log(`### [${post.title}](/post/${post.slot})\n\n发表于${time}。\n`);
 
   // console.log(post.title, time);
+  /*
   console.log(post.tags);
-  // mkdirSync(`./src/routes/(black-history)/post/(2022)/${post.slot}`, {
-  //   recursive: true,
-  // });
-  // writeFileSync(
-  //   `./src/routes/(black-history)/post/(2022)/${post.slot}/index.mdx`,
-  //   post.content
-  // );
+  mkdirSync(`./src/routes/(black-history)/post/(2021)/${post.slot}`, {
+    recursive: true,
+  });
+  writeFileSync(
+    `./src/routes/(black-history)/post/(2021)/${post.slot}/index.mdx`,
+    
+    `---
+title: ${post.title}
+---
+
+# ${post.title}
+
+<div class="info">
+  <span>swwind</span>
+  <time>${time}</time>${post.tags.map((tag) => `\n  <span>#${tag}</span>`).join('')}
+</div>
+
+${post.content.replace('<!-- more -->', '')}`
+  );
+  */
 }
