9bb9f375a78ccf67bdaebc3da83a0f3649a57eaa
swwind <i@sww.moe>
2023-05-29 10:38:13 +0800

    remove Link -> a

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,4 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
 
 import { header } from "~/metadata.json";
 
@@ -7,18 +6,18 @@ export const Header = component$(() => {
   return (
     <header class="mx-6 mb-4 flex shrink-0 justify-between pt-6">
       <span>
-        <Link href="/" class="font-serif hover:underline">
+        <a href="/" class="font-serif hover:underline">
           {header}
-        </Link>
+        </a>
       </span>
 
       <span class="inline-flex gap-4">
-        <Link href="/about" class="font-serif hover:underline">
+        <a href="/about" class="font-serif hover:underline">
           关于
-        </Link>
-        <Link href="/friend" class="font-serif hover:underline">
+        </a>
+        <a href="/friend" class="font-serif hover:underline">
           友链
-        </Link>
+        </a>
       </span>
     </header>
   );
