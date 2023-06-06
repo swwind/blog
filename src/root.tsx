36a04beeb5ac6e63a97675a7dbf06eaa2fab37dc
swwind <i@sww.moe>
2023-06-06 14:29:27 +0800

    upgrade qwik version

--- src/root.tsx
+++ src/root.tsx
@@ -2,7 +2,6 @@ import { component$, useStyles$ } from "@builder.io/qwik";
 import {
   QwikCityProvider,
   RouterOutlet,
-  ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
@@ -30,7 +29,6 @@ export default component$(() => {
       </head>
       <body lang="en">
         <RouterOutlet />
-        <ServiceWorkerRegister />
       </body>
     </QwikCityProvider>
   );
