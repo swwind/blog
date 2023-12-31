348e888ce994c3081d62f4d8fc3a83d601cfdc42
swwind <i@sww.moe>
2024-01-01 00:58:21 +0800

    Update 2024

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = component$(() => {
   return (
     <footer class="mx-6 flex shrink-0 justify-end py-6">
       <p class="text-right font-serif text-xs opacity-60">
-        <span>Copyright © 2017-2023 swwind. All rights reserved</span>
+        <span>Copyright © 2017-2024 swwind. All rights reserved</span>
         <br />
         <span>
           Except where otherwise noted, content on this blog is licensed under
