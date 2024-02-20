e394743082e2ff1300acaeffa8a7887ccbccd0ff
swwind <i@sww.moe>
2024-02-20 16:19:23 +0800

    make website faster

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,8 @@ export default function () {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="shortcut icon" href="/momoi.png" />
+        <link rel="shortcut icon" href="/momoi.webp" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <RouterHead />
       </head>
       <body>
