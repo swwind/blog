a04495093fe915f1510da7248197f36418650b83
swwind <i@sww.moe>
2024-04-16 19:20:35 +0800

    add new post

--- app/components/metadata/metadata.tsx
+++ app/components/metadata/metadata.tsx
@@ -4,16 +4,43 @@ interface MetadataProps {
   author: string;
   time: string;
   tags?: string;
+  outdate?: boolean;
 }
 
 export const Metadata = (props: MetadataProps) => {
   const time = new Date(props.time);
+  const yearsOld = Math.floor(
+    (Date.now() - time.getTime()) / (365 * 24 * 60 * 60 * 1000),
+  );
 
   return (
-    <div class="info">
-      <span>{props.author}</span>
-      <time>{toYangliCalendar(time)}</time>
-      {props.tags?.split(",").map((x, i) => <span key={i}>#{x}</span>)}
-    </div>
+    <>
+      <div class="info">
+        <span>{props.author}</span>
+        <time>{toYangliCalendar(time)}</time>
+        {props.tags?.split(",").map((x, i) => <span key={i}>#{x}</span>)}
+      </div>
+
+      {props.outdate !== false && yearsOld > 0 && (
+        <>
+          <hr />
+          <p>
+            <b>
+              注意：这篇文章已经发布超过 {yearsOld}{" "}
+              年，世界线的变动可能会导致故事走向
+              <a
+                href="https://moegirl.uk/Bad_End"
+                referrerpolicy="no-referrer"
+                target="_blank"
+              >
+                不同的结局
+              </a>
+              。
+            </b>
+          </p>
+          <hr />
+        </>
+      )}
+    </>
   );
 };
