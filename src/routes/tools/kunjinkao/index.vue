2d255e222e93d49c3f1af9b4606df309317b7d13
swwind <i@sww.moe>
2025-08-25 22:06:16 +0800

    remove useless encodings

--- src/routes/tools/kunjinkao/index.vue
+++ src/routes/tools/kunjinkao/index.vue
@@ -3,7 +3,7 @@
     <h2>锟斤拷生成器</h2>
 
     <p class="flex flex-wrap items-baseline gap-4">
-      <span>原文</span>
+      <span>原　文</span>
       <input type="text" class="w-64 font-mono" v-model="input" />
     </p>
 
@@ -37,16 +37,6 @@
       <span>{{ kunkaoma }}</span>
     </p>
 
-    <p class="flex flex-wrap items-baseline gap-4">
-      <span>烫烫烫</span>
-      <span>{{ tangtangtang }}</span>
-    </p>
-
-    <p class="flex flex-wrap items-baseline gap-4">
-      <span>屯屯屯</span>
-      <span>{{ tuntuntun }}</span>
-    </p>
-
     <h3>说明</h3>
 
     <ul>
@@ -60,8 +50,6 @@
       <li>
         锟拷码：以 GBK 的方式读取 UTF-8 编码的字符串，再以 UTF-8 的方式重新读取
       </li>
-      <li>烫烫烫：内存全为 0xCC</li>
-      <li>屯屯屯：内存全为 0xCD</li>
     </ul>
   </div>
 </template>
@@ -82,8 +70,6 @@ const fuhaoma = ref("");
 const pinyinma = ref("");
 const wenjuma = ref("");
 const kunkaoma = ref("");
-const tangtangtang = ref("");
-const tuntuntun = ref("");
 
 function decodeGBK(buffer: Uint8Array) {
   return new TextDecoder("gbk").decode(buffer);
@@ -108,12 +94,5 @@ watch(input, (input) => {
   pinyinma.value = decodeAscii(encodeGBK(input));
   wenjuma.value = decodeUtf8(encodeGBK(decodeGBK(encodeUtf8(input))));
   kunkaoma.value = decodeGBK(encodeUtf8(decodeUtf8(encodeGBK(input))));
-
-  tangtangtang.value = decodeGBK(
-    new Uint8Array(new Array(input.length * 2).fill(0xcc)),
-  );
-  tuntuntun.value = decodeGBK(
-    new Uint8Array(new Array(input.length * 2).fill(0xcd)),
-  );
 });
 </script>
